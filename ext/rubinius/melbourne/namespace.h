4d6ebc1a539c565342a4123c05fb7a695b42b35a
Brian Shirai <brixen@gmail.com>
2013-08-20 21:57:53 -0700

    Regen'd namespace.

--- ext/rubinius/melbourne/namespace.h
+++ ext/rubinius/melbourne/namespace.h
@@ -1,3 +1,3 @@
-#define MELBOURNE                 melbourne_2_0_0_0
-#define MELBOURNE_FILE_TO_AST     melbourne_2_0_0_0_file_to_ast
-#define MELBOURNE_STRING_TO_AST   melbourne_2_0_0_0_string_to_ast
+#define MELBOURNE                 melbourne_2_0_0_1
+#define MELBOURNE_FILE_TO_AST     melbourne_2_0_0_1_file_to_ast
+#define MELBOURNE_STRING_TO_AST   melbourne_2_0_0_1_string_to_ast
