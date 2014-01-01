257e27c511bffa5edb058bec3e764c11bb945b6a
Brian Shirai <brixen@gmail.com>
2013-12-31 18:44:09 -0800

    Removed relates from yield specs.

--- spec/yield_spec.rb
+++ spec/yield_spec.rb
@@ -1,65 +1,45 @@
 describe "A Yield node" do
-  relates "yield(42, 24)" do
-    parse do
-      [:yield, [:lit, 42], [:lit, 24]]
-    end
+  parse "yield(42, 24)" do
+    [:yield, [:lit, 42], [:lit, 24]]
   end
 
-  relates "yield([42, 24])" do
-    parse do
-      [:yield, [:array, [:lit, 42], [:lit, 24]]]
-    end
+  parse "yield([42, 24])" do
+    [:yield, [:array, [:lit, 42], [:lit, 24]]]
   end
 
-  relates "yield([42])" do
-    parse do
-      [:yield, [:array, [:lit, 42]]]
-    end
+  parse "yield([42])" do
+    [:yield, [:array, [:lit, 42]]]
   end
 
-  relates "yield([])" do
-    parse do
-      [:yield, [:array]]
-    end
+  parse "yield([])" do
+    [:yield, [:array]]
   end
 
-  relates "yield(42)" do
-    parse do
-      [:yield, [:lit, 42]]
-    end
+  parse "yield(42)" do
+    [:yield, [:lit, 42]]
   end
 
-  relates "yield" do
-    parse do
-      [:yield]
-    end
+  parse "yield" do
+    [:yield]
   end
 
-  relates "yield([*[]])" do
+  parse "yield([*[]])" do
     # TODO
   end
 
-  relates "yield([*[1]])" do
-    parse do
-      [:yield, [:array, [:splat, [:array, [:lit, 1]]]]]
-    end
+  parse "yield([*[1]])" do
+    [:yield, [:array, [:splat, [:array, [:lit, 1]]]]]
   end
 
-  relates "yield([*[1, 2]])" do
-    parse do
-      [:yield, [:array, [:splat, [:array, [:lit, 1], [:lit, 2]]]]]
-    end
+  parse "yield([*[1, 2]])" do
+    [:yield, [:array, [:splat, [:array, [:lit, 1], [:lit, 2]]]]]
   end
 
-  relates "yield(*[1])" do
-    parse do
-      [:yield, [:splat, [:array, [:lit, 1]]]]
-    end
+  parse "yield(*[1])" do
+    [:yield, [:splat, [:array, [:lit, 1]]]]
   end
 
-  relates "yield(*[1, 2])" do
-    parse do
-      [:yield, [:splat, [:array, [:lit, 1], [:lit, 2]]]]
-    end
+  parse "yield(*[1, 2])" do
+    [:yield, [:splat, [:array, [:lit, 1], [:lit, 2]]]]
   end
 end
