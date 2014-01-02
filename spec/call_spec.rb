8a8be3b199a2df46810d9b6a0b2db6e6163d9d34
Brian Shirai <brixen@gmail.com>
2014-01-02 15:39:24 -0800

    Updated call specs.

--- spec/call_spec.rb
+++ spec/call_spec.rb
@@ -1,268 +1,184 @@
 describe "A Call node" do
-  relates "self.method" do
-    parse do
-      [:call, [:self], :method, [:arglist]]
-    end
+  parse "self.method" do
+    [:call, [:self], :method, [:arglist]]
   end
 
-  relates "1.m(2)" do
-    parse do
-      [:call, [:lit, 1], :m, [:arglist, [:lit, 2]]]
-    end
+  parse "1.m(2)" do
+    [:call, [:lit, 1], :m, [:arglist, [:lit, 2]]]
   end
 
-  relates "h(1, 2, *a)" do
-    parse do
-      [:call, nil, :h,
-       [:arglist,
-         [:lit, 1],
-         [:lit, 2],
-         [:splat, [:call, nil, :a, [:arglist]]]]]
-    end
+  parse "h(1, 2, *a)" do
+    [:call, nil, :h,
+     [:arglist,
+       [:lit, 1],
+       [:lit, 2],
+       [:splat, [:call, nil, :a, [:arglist]]]]]
   end
 
-  relates <<-ruby do
+  parse <<-ruby do
       begin
         (1 + 1)
       end
     ruby
 
-    parse do
-      [:call, [:lit, 1], :+, [:arglist, [:lit, 1]]]
-    end
+    [:call, [:lit, 1], :+, [:arglist, [:lit, 1]]]
   end
 
-  relates "blah(*a)" do
-    parse do
-      [:call, nil, :blah,
-        [:arglist, [:splat, [:call, nil, :a, [:arglist]]]]]
-    end
+  parse "blah(*a)" do
+    [:call, nil, :blah,
+      [:arglist, [:splat, [:call, nil, :a, [:arglist]]]]]
   end
 
-  relates "a.b(&c)" do
-    parse do
-      [:call,
-       [:call, nil, :a, [:arglist]],
-       :b,
-       [:arglist, [:block_pass, [:call, nil, :c, [:arglist]]]]]
-    end
+  parse "a.b(&c)" do
+    [:call,
+     [:call, nil, :a, [:arglist]],
+     :b,
+     [:arglist, [:block_pass, [:call, nil, :c, [:arglist]]]]]
   end
 
-  relates "a.b(4, &c)" do
-    parse do
-      [:call,
-       [:call, nil, :a, [:arglist]],
-       :b,
-       [:arglist, [:lit, 4], [:block_pass, [:call, nil, :c, [:arglist]]]]]
-    end
+  parse "a.b(4, &c)" do
+    [:call,
+     [:call, nil, :a, [:arglist]],
+     :b,
+     [:arglist, [:lit, 4], [:block_pass, [:call, nil, :c, [:arglist]]]]]
   end
 
-  relates "a.b(1, 2, 3, &c)" do
-    parse do
-      [:call,
-       [:call, nil, :a, [:arglist]],
-       :b,
-       [:arglist,
-        [:lit, 1],
-        [:lit, 2],
-        [:lit, 3],
-        [:block_pass, [:call, nil, :c, [:arglist]]]]]
-    end
+  parse "a.b(1, 2, 3, &c)" do
+    [:call,
+     [:call, nil, :a, [:arglist]],
+     :b,
+     [:arglist,
+      [:lit, 1],
+      [:lit, 2],
+      [:lit, 3],
+      [:block_pass, [:call, nil, :c, [:arglist]]]]]
   end
 
-  relates "a(&b)" do
-    parse do
-      [:call,
-       nil,
-       :a,
-       [:arglist, [:block_pass, [:call, nil, :b, [:arglist]]]]]
-    end
+  parse "a(&b)" do
+    [:call,
+     nil,
+     :a,
+     [:arglist, [:block_pass, [:call, nil, :b, [:arglist]]]]]
   end
 
-  relates "a(4, &b)" do
-    parse do
-      [:call,
-       nil,
-       :a,
-       [:arglist, [:lit, 4], [:block_pass, [:call, nil, :b, [:arglist]]]]]
-    end
+  parse "a(4, &b)" do
+    [:call,
+     nil,
+     :a,
+     [:arglist, [:lit, 4], [:block_pass, [:call, nil, :b, [:arglist]]]]]
   end
 
-  relates "a(1, 2, 3, &b)" do
-    parse do
-      [:call,
-       nil,
-       :a,
-       [:arglist,
-        [:lit, 1],
-        [:lit, 2],
-        [:lit, 3],
-        [:block_pass, [:call, nil, :b, [:arglist]]]]]
-    end
+  parse "a(1, 2, 3, &b)" do
+    [:call,
+     nil,
+     :a,
+     [:arglist,
+      [:lit, 1],
+      [:lit, 2],
+      [:lit, 3],
+      [:block_pass, [:call, nil, :b, [:arglist]]]]]
   end
 
-  relates "define_attr_method(:x, :sequence_name, &Proc.new { |*args| nil })" do
-    parse do
-      [:call,
-       nil,
-       :define_attr_method,
-       [:arglist,
-        [:lit, :x],
-        [:lit, :sequence_name],
-        [:block_pass,
-         [:iter,
-          [:call, [:const, :Proc], :new, [:arglist]],
-          [:masgn, [:array, [:splat, [:lasgn, :args]]]],
-          [:nil]]]]]
-    end
+  parse "define_attr_method(:x, :sequence_name, &Proc.new { |*args| nil })" do
+    [:call,
+     nil,
+     :define_attr_method,
+     [:arglist,
+      [:lit, :x],
+      [:lit, :sequence_name],
+      [:block_pass,
+       [:call,
+        [:const, :Proc],
+        :new,
+        [:arglist, [:iter, [:args, :"*args"], [:nil]]]]]]]
   end
 
-  relates "r.read_body(dest, &block)" do
-    parse do
-      [:call,
-       [:call, nil, :r, [:arglist]],
-       :read_body,
-       [:arglist,
-        [:call, nil, :dest, [:arglist]],
-        [:block_pass, [:call, nil, :block, [:arglist]]]]]
-    end
+  parse "r.read_body(dest, &block)" do
+    [:call,
+     [:call, nil, :r, [:arglist]],
+     :read_body,
+     [:arglist,
+      [:call, nil, :dest, [:arglist]],
+      [:block_pass, [:call, nil, :block, [:arglist]]]]]
   end
 
-  relates "o.m(:a => 1, :b => 2)" do
-    parse do
-      [:call,
-       [:call, nil, :o, [:arglist]],
-       :m,
-       [:arglist, [:hash, [:lit, :a], [:lit, 1], [:lit, :b], [:lit, 2]]]]
-    end
+  parse "o.m(:a => 1, :b => 2)" do
+    [:call,
+     [:call, nil, :o, [:arglist]],
+     :m,
+     [:arglist, [:hash, [:lit, :a], [:lit, 1], [:lit, :b], [:lit, 2]]]]
   end
 
-  relates "o.m(42, :a => 1, :b => 2)" do
-    parse do
-      [:call,
-       [:call, nil, :o, [:arglist]],
-       :m,
-       [:arglist,
-        [:lit, 42],
-        [:hash, [:lit, :a], [:lit, 1], [:lit, :b], [:lit, 2]]]]
-    end
+  parse "o.m(42, :a => 1, :b => 2)" do
+    [:call,
+     [:call, nil, :o, [:arglist]],
+     :m,
+     [:arglist,
+      [:lit, 42],
+      [:hash, [:lit, :a], [:lit, 1], [:lit, :b], [:lit, 2]]]]
   end
 
-  relates "o.m(42, :a => 1, :b => 2, *c)" do
-    parse do
-      [:call,
-       [:call, nil, :o, [:arglist]],
-       :m,
-       [:arglist,
-        [:lit, 42],
-        [:hash, [:lit, :a], [:lit, 1], [:lit, :b], [:lit, 2]],
-        [:splat, [:call, nil, :c, [:arglist]]]]]
-    end
+  parse "o.puts(42)" do
+    [:call, [:call, nil, :o, [:arglist]], :puts, [:arglist, [:lit, 42]]]
   end
 
-  relates "a (1,2,3)" do
-    parse do
-      [:call, nil, :a, [:arglist, [:lit, 1], [:lit, 2], [:lit, 3]]]
-    end
+  parse "1.b(c)" do
+    [:call, [:lit, 1], :b, [:arglist, [:call, nil, :c, [:arglist]]]]
   end
 
-  relates "o.puts(42)" do
-    parse do
-      [:call, [:call, nil, :o, [:arglist]], :puts, [:arglist, [:lit, 42]]]
-    end
+  parse "(v = (1 + 1)).zero?" do
+    [:call,
+     [:lasgn, :v, [:call, [:lit, 1], :+, [:arglist, [:lit, 1]]]],
+     :zero?,
+     [:arglist]]
   end
 
-  relates "1.b(c)" do
-    parse do
-      [:call, [:lit, 1], :b, [:arglist, [:call, nil, :c, [:arglist]]]]
-    end
+  parse "-2**31" do
+    [:call,
+     [:call, [:lit, 2], :**, [:arglist, [:lit, 31]]],
+     :-@,
+     [:arglist]]
   end
 
-  relates "(v = (1 + 1)).zero?" do
-    parse do
-      [:call,
-       [:lasgn, :v, [:call, [:lit, 1], :+, [:arglist, [:lit, 1]]]],
-       :zero?,
-       [:arglist]]
-    end
+  parse "a[]" do
+    [:call, [:call, nil, :a, [:arglist]], :[], [:arglist]]
   end
 
-  relates "-2**31" do
-    parse do
-      [:call,
-       [:call, [:lit, 2], :**, [:arglist, [:lit, 31]]],
-       :-@,
-       [:arglist]]
-    end
+  parse "m(:a => 1, :b => 2)" do
+    [:call,
+     nil,
+     :m,
+     [:arglist, [:hash, [:lit, :a], [:lit, 1], [:lit, :b], [:lit, 2]]]]
   end
 
-  relates "a[]" do
-    parse do
-      [:call, [:call, nil, :a, [:arglist]], :[], [:arglist]]
-    end
+  parse "m(42, :a => 1, :b => 2)" do
+    [:call,
+     nil,
+     :m,
+     [:arglist, [:lit, 42], [:hash, [:lit, :a], [:lit, 1], [:lit, :b], [:lit, 2]]]]
   end
 
-  relates "m(:a => 1, :b => 2)" do
-    parse do
-      [:call,
-       nil,
-       :m,
-       [:arglist, [:hash, [:lit, :a], [:lit, 1], [:lit, :b], [:lit, 2]]]]
-    end
+  parse "m(42)" do
+    [:call, nil, :m, [:arglist, [:lit, 42]]]
   end
 
-  relates "m(42, :a => 1, :b => 2)" do
-    parse do
-      [:call,
-       nil,
-       :m,
-       [:arglist, [:lit, 42], [:hash, [:lit, :a], [:lit, 1], [:lit, :b], [:lit, 2]]]]
-    end
+  parse "a(:b) { :c }" do
+    [:call, nil, :a, [:arglist, [:lit, :b], [:iter, [:args], [:lit, :c]]]]
   end
 
-  relates "m(42, :a => 1, :b => 2, *c)" do
-    parse do
-      [:call,
-       nil,
-       :m,
-       [:arglist,
-        [:lit, 42],
-        [:hash, [:lit, :a], [:lit, 1], [:lit, :b], [:lit, 2]],
-        [:splat, [:call, nil, :c, [:arglist]]]]]
-    end
+  parse "a [42]" do
+    [:call, nil, :a, [:arglist, [:array, [:lit, 42]]]]
   end
 
-  relates "m(42)" do
-    parse do
-      [:call, nil, :m, [:arglist, [:lit, 42]]]
-    end
+  parse "42 if block_given?" do
+    [:if, [:call, nil, :block_given?, [:arglist]], [:lit, 42], nil]
   end
 
-  relates "a(:b) { :c }" do
-    parse do
-      [:iter, [:call, nil, :a, [:arglist, [:lit, :b]]], nil, [:lit, :c]]
-    end
+  parse "method" do
+    [:call, nil, :method, [:arglist]]
   end
 
-  relates "a [42]" do
-    parse do
-      [:call, nil, :a, [:arglist, [:array, [:lit, 42]]]]
-    end
-  end
-
-  relates "42 if block_given?" do
-    parse do
-      [:if, [:call, nil, :block_given?, [:arglist]], [:lit, 42], nil]
-    end
-  end
-
-  relates "method" do
-    parse do
-      [:call, nil, :method, [:arglist]]
-    end
-  end
-
-  relates <<-ruby do
+  parse <<-ruby do
       a << begin
              b
            rescue
@@ -270,29 +186,25 @@ describe "A Call node" do
            end
     ruby
 
-    parse do
-      [:call,
-       [:call, nil, :a, [:arglist]],
-       :<<,
-       [:arglist,
-        [:rescue,
-         [:call, nil, :b, [:arglist]],
-         [:resbody, [:array], [:call, nil, :c, [:arglist]]]]]]
-    end
+    [:call,
+     [:call, nil, :a, [:arglist]],
+     :<<,
+     [:arglist,
+      [:rescue,
+       [:call, nil, :b, [:arglist]],
+       [:resbody,
+        [:array, [:const, :StandardError]],
+        [:call, nil, :c, [:arglist]]]]]]
   end
 
-  relates "meth([*[1]])" do
-    parse do
-      [:call,
-        nil,
-        :meth,
-        [:arglist, [:array, [:splat, [:array, [:lit, 1]]]]]]
-    end
+  parse "meth([*[1]])" do
+    [:call,
+      nil,
+      :meth,
+      [:arglist, [:splat, [:array, [:lit, 1]]]]]
   end
 
-  relates "meth(*[1])" do
-    parse do
-      [:call, nil, :meth, [:arglist, [:splat, [:array, [:lit, 1]]]]]
-    end
+  parse "meth(*[1])" do
+    [:call, nil, :meth, [:arglist, [:splat, [:array, [:lit, 1]]]]]
   end
 end
