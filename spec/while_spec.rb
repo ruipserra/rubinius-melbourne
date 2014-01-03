7a4da6205074bd0a40e4ba07261780e0adc45fe0
Brian Shirai <brixen@gmail.com>
2014-01-02 20:23:58 -0800

    Updated while specs.

--- spec/while_spec.rb
+++ spec/while_spec.rb
@@ -6,163 +6,69 @@ describe "A While node" do
      true
   ]
 
-  relates <<-ruby do
+  parse <<-ruby do
       while a
         b + 1
       end
     ruby
 
-    parse do
-      pre_while_sexp
-    end
+    pre_while_sexp
   end
 
-  relates <<-ruby do
+  parse <<-ruby do
       a = x
       while a.b
         1
       end
     ruby
 
-    parse do
-      [:block,
-       [:lasgn, :a, [:call, nil, :x, [:arglist]]],
-       [:while, [:call, [:lvar, :a], :b, [:arglist]], [:lit, 1], true]]
-    end
+    [:block,
+     [:lasgn, :a, [:call, nil, :x, [:arglist]]],
+     [:while, [:call, [:lvar, :a], :b, [:arglist]], [:lit, 1], true]]
   end
 
-  relates "b + 1 while a" do
-    parse do
-      pre_while_sexp
-    end
+  parse "b + 1 while a" do
+    pre_while_sexp
   end
 
-  relates <<-ruby do
-      until not a
-        b + 1
-      end
-    ruby
-
-    parse do
-      pre_while_sexp
-    end
-  end
-
-  relates "b + 1 until not a" do
-    parse do
-      pre_while_sexp
-    end
-  end
-
-  post_while_sexp = [
-    :while,
-     [:call, nil, :a, [:arglist]],
-     [:call, [:call, nil, :b, [:arglist]], :+, [:arglist, [:lit, 1]]],
-     false
-  ]
-
-  post_while = lambda do |g|
-    top    = g.new_label
-    rdo    = g.new_label
-    brk    = g.new_label
-    bottom = g.new_label
-
-    g.push_modifiers
-
-    top.set!
-
-    g.push :self
-    g.send :b, 0, true
-    g.push 1
-    g.send :+, 1, false
-    g.pop
-
-    rdo.set!
-
-    g.push :self
-    g.send :a, 0, true
-    g.gif bottom
-
-    g.check_interrupts
-    g.goto top
-
-    bottom.set!
-    g.push :nil
-
-    brk.set!
-    g.pop_modifiers
-  end
-
-  relates <<-ruby do
+  parse <<-ruby do
       begin
         b + 1
       end while a
     ruby
 
-    parse do
-      post_while_sexp
-    end
-  end
-
-  relates <<-ruby do
-      begin
-        b + 1
-      end until not a
-    ruby
-
-    parse do
-      post_while_sexp
-    end
+    [:while,
+     [:call, nil, :a, [:arglist]],
+     [:call, [:call, nil, :b, [:arglist]], :+, [:arglist, [:lit, 1]]],
+     false]
   end
 
   nil_condition_sexp = [:while, [:nil], [:call, nil, :a, [:arglist]], true]
 
-  relates "a while ()" do
-    parse do
-      nil_condition_sexp
-    end
+  parse "a while ()" do
+    nil_condition_sexp
   end
 
-  relates <<-ruby do
+  parse <<-ruby do
       while ()
         a
       end
     ruby
 
-    parse do
-      nil_condition_sexp
-    end
+    nil_condition_sexp
   end
 
-  relates "a until not ()" do
-
-    parse do
-      nil_condition_sexp
-    end
-  end
-
-  relates <<-ruby do
-      until not ()
-        a
-      end
-    ruby
-
-    parse do
-      nil_condition_sexp
-    end
-  end
-
-  relates <<-ruby do
+  parse <<-ruby do
       while 1
         2
         break :brk
       end
     ruby
 
-    # TODO
+    [:while, [:lit, 1], [:block, [:lit, 2], [:break, [:lit, :brk]]], true]
   end
 
-  relates <<-ruby do
+  parse <<-ruby do
       while 1
         begin
           2
@@ -173,10 +79,15 @@ describe "A While node" do
       end
     ruby
 
-    # TODO
+    [:while,
+     [:lit, 1],
+     [:rescue,
+      [:block, [:lit, 2], [:break, [:lit, :brk]]],
+      [:resbody, [:array, [:const, :StandardError]], [:lit, 3]]],
+     true]
   end
 
-  relates <<-ruby do
+  parse <<-ruby do
       while 1
         begin
           2
@@ -187,6 +98,13 @@ describe "A While node" do
       end
     ruby
 
-    # TODO
+    [:while,
+     [:lit, 1],
+     [:rescue,
+      [:lit, 2],
+      [:resbody,
+       [:array, [:const, :StandardError]],
+       [[:lit, 3], [:break, [:lit, :brk]]]]],
+     true]
   end
 end
