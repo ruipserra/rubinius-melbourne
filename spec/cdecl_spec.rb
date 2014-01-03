9932ef800f4cb7c970c0f126e018ef702cf97245
Brian Shirai <brixen@gmail.com>
2014-01-02 18:45:35 -0800

    Updated cdecl specs.

--- spec/cdecl_spec.rb
+++ spec/cdecl_spec.rb
@@ -1,45 +1,33 @@
 describe "A Cdecl node" do
-  relates "X = 42" do
-    parse do
-      [:cdecl, :X, [:lit, 42]]
-    end
+  parse "X = 42" do
+    [:cdecl, :X, [:lit, 42]]
   end
 
-  relates "::X = 1" do
-    parse do
-      [:cdecl, [:colon3, :X], [:lit, 1]]
-    end
+  parse "::X = 1" do
+    [:cdecl, [:colon3, :X], [:lit, 1]]
   end
 
-  relates "X::Y = 1" do
-    parse do
-      [:cdecl, [:colon2, [:const, :X], :Y], [:lit, 1]]
-    end
+  parse "X::Y = 1" do
+    [:cdecl, [:colon2, [:const, :X], :Y], [:lit, 1]]
   end
 
-  relates "X::Y::Z = a" do
-    parse do
-      [:cdecl,
-       [:colon2, [:colon2, [:const, :X], :Y], :Z],
-       [:call, nil, :a, [:arglist]]]
-    end
+  parse "X::Y::Z = a" do
+    [:cdecl,
+     [:colon2, [:colon2, [:const, :X], :Y], :Z],
+     [:call, nil, :a, [:arglist]]]
   end
 
-  relates "a::A = 1" do
-    parse do
-      [:cdecl, [:colon2, [:call, nil, :a, [:arglist]], :A], [:lit, 1]]
-    end
+  parse "a::A = 1" do
+    [:cdecl, [:colon2, [:call, nil, :a, [:arglist]], :A], [:lit, 1]]
   end
 
-  relates <<-ruby do
+  parse <<-ruby do
       a = Object
       a::B = b
     ruby
 
-    parse do
-      [:block,
-       [:lasgn, :a, [:const, :Object]],
-       [:cdecl, [:colon2, [:lvar, :a], :B], [:call, nil, :b, [:arglist]]]]
-    end
+    [:block,
+     [:lasgn, :a, [:const, :Object]],
+     [:cdecl, [:colon2, [:lvar, :a], :B], [:call, nil, :b, [:arglist]]]]
   end
 end
