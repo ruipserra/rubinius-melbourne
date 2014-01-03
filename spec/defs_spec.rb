2b046e324240680dcbdd180ea4a3536388ed2e65
Brian Shirai <brixen@gmail.com>
2014-01-02 19:47:39 -0800

    Updated defs specs.

--- spec/defs_spec.rb
+++ spec/defs_spec.rb
@@ -1,20 +1,18 @@
 describe "A Defs node" do
-  relates <<-ruby do
+  parse <<-ruby do
       def self.x(y)
         (y + 1)
       end
     ruby
 
-    parse do
-      [:defs,
-         [:self],
-         :x,
-         [:args, :y],
-         [:scope, [:block, [:call, [:lvar, :y], :+, [:arglist, [:lit, 1]]]]]]
-    end
+    [:defs,
+       [:self],
+       :x,
+       [:args, :y],
+       [:scope, [:block, [:call, [:lvar, :y], :+, [:arglist, [:lit, 1]]]]]]
   end
 
-  relates <<-ruby do
+  parse <<-ruby do
       def self.setup(ctx)
         bind = allocate
         bind.context = ctx
@@ -22,60 +20,54 @@ describe "A Defs node" do
       end
     ruby
 
-    parse do
-      [:defs,
-         [:self],
-         :setup,
-         [:args, :ctx],
-         [:scope,
-          [:block,
-           [:lasgn, :bind, [:call, nil, :allocate, [:arglist]]],
-           [:attrasgn, [:lvar, :bind], :context=, [:arglist, [:lvar, :ctx]]],
-           [:return, [:lvar, :bind]]]]]
-    end
+    [:defs,
+       [:self],
+       :setup,
+       [:args, :ctx],
+       [:scope,
+        [:block,
+         [:lasgn, :bind, [:call, nil, :allocate, [:arglist]]],
+         [:attrasgn, [:lvar, :bind], :context=, [:arglist, [:lvar, :ctx]]],
+         [:return, [:lvar, :bind]]]]]
   end
 
-  relates <<-ruby do
+  parse <<-ruby do
       def self.empty(*)
       end
     ruby
 
-    parse do
-      [:defs, [:self], :empty, [:args, :*], [:scope, [:block]]]
-    end
+    [:defs, [:self], :empty, [:args, :*], [:scope, [:block, [:nil]]]]
   end
 
-  relates <<-ruby do
+  parse <<-ruby do
       def self.empty
       end
     ruby
 
-    parse do
-      [:defs, [:self], :empty, [:args], [:scope, [:block]]]
-    end
+    [:defs, [:self], :empty, [:args], [:scope, [:block, [:nil]]]]
   end
 
-  relates <<-ruby do
+  parse <<-ruby do
       def (a.b).empty(*)
       end
     ruby
 
-    parse do
-      [:defs,
-       [:call, [:call, nil, :a, [:arglist]], :b, [:arglist]],
-       :empty,
-       [:args, :*],
-       [:scope, [:block]]]
-    end
+    [:defs,
+     [:call, [:call, nil, :a, [:arglist]], :b, [:arglist]],
+     :empty,
+     [:args, :*],
+     [:scope, [:block, [:nil]]]]
   end
 
-  relates <<-ruby do
+  parse <<-ruby do
     x = "a"
     def x.m(a)
       a
     end
     ruby
 
-    # TODO
+    [:block,
+     [:lasgn, :x, [:str, "a"]],
+     [:defs, [:lvar, :x], :m, [:args, :a], [:scope, [:block, [:lvar, :a]]]]]
   end
 end
