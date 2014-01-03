0b14550c77d44676da48857941d0630d88287184
Brian Shirai <brixen@gmail.com>
2014-01-03 15:11:31 -0800

    Updated returns specs.

--- spec/return_spec.rb
+++ spec/return_spec.rb
@@ -1,57 +1,40 @@
 describe "A Return node" do
-  relates "return" do
-    parse do
-      [:return]
-    end
+  parse "return" do
+    [:return]
   end
 
-  relates "return 1" do
-    parse do
-      [:return, [:lit, 1]]
-    end
+  parse "return 1" do
+    [:return, [:lit, 1]]
   end
 
-  relates "return *1" do
-    parse do
-      [:return, [:svalue, [:splat, [:lit, 1]]]]
-    end
+  parse "return *1" do
+    [:return, [:splat, [:lit, 1]]]
   end
 
-  relates <<-ruby do
+  parse <<-ruby do
       x = 1, 2
       return *x
     ruby
 
-    parse do
-      [:block,
-        [:lasgn, :x, [:svalue, [:array, [:lit, 1], [:lit, 2]]]],
-        [:return, [:svalue, [:splat, [:lvar, :x]]]]]
-    end
+    [:block,
+      [:lasgn, :x, [:array, [:lit, 1], [:lit, 2]]],
+      [:return, [:splat, [:lvar, :x]]]]
   end
 
-  relates "return 1, 2, 3" do
-    parse do
-      [:return, [:array, [:lit, 1], [:lit, 2], [:lit, 3]]]
-    end
+  parse "return 1, 2, 3" do
+    [:return, [:array, [:lit, 1], [:lit, 2], [:lit, 3]]]
   end
 
-  relates "return 1, 2, *c" do
-    parse do
-      [:return,
-       [:array,
-         [:lit, 1], [:lit, 2], [:splat, [:call, nil, :c, [:arglist]]]]]
-    end
+  parse "return 1, 2, *c" do
+    [:return,
+     [:argscat, [:array, [:lit, 1], [:lit, 2]], [:call, nil, :c, [:arglist]]]]
   end
 
-  relates "return [*[1]]" do
-    parse do
-      [:return, [:array, [:splat, [:array, [:lit, 1]]]]]
-    end
+  parse "return [*[1]]" do
+    [:return, [:splat, [:array, [:lit, 1]]]]
   end
 
-  relates "return *[1]" do
-    parse do
-      [:return, [:svalue, [:splat, [:array, [:lit, 1]]]]]
-    end
+  parse "return *[1]" do
+    [:return, [:splat, [:array, [:lit, 1]]]]
   end
 end
