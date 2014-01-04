b57ba7229e016e48b6754f67f32c90d22efe2a1d
Brian Shirai <brixen@gmail.com>
2014-01-03 16:05:08 -0800

    Updated hash specs.

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -1,31 +1,27 @@
 describe "A Hash node" do
-  relates "{ 1 => 2, 3 => 4 }" do
-    parse do
-      [:hash, [:lit, 1], [:lit, 2], [:lit, 3], [:lit, 4]]
-    end
+  parse "{ 1 => 2, 3 => 4 }" do
+    [:hash, [:lit, 1], [:lit, 2], [:lit, 3], [:lit, 4]]
   end
 
-  relates "{ 1 => (2 rescue 3) }" do
-    parse do
-      [:hash, [:lit, 1], [:rescue, [:lit, 2], [:resbody, [:array], [:lit, 3]]]]
-    end
+  parse "{ 1 => (2 rescue 3) }" do
+    [:hash,
+     [:lit, 1],
+     [:rescue,
+      [:lit, 2],
+      [:resbody, [:array, [:const, :StandardError]], [:lit, 3]]]]
   end
 
-  relates "{ 1 => [*1] }" do
-    parse do
-      [:hash, [:lit, 1], [:array, [:splat, [:lit, 1]]]]
-    end
+  parse "{ 1 => [*1] }" do
+    [:hash, [:lit, 1], [:splat, [:lit, 1]]]
   end
 
-  relates <<-ruby do
+  parse <<-ruby do
       a = 1
       { :a => a }
     ruby
 
-    parse do
-      [:block,
-        [:lasgn, :a, [:lit, 1]],
-        [:hash, [:lit, :a], [:lvar, :a]]]
-    end
+    [:block,
+      [:lasgn, :a, [:lit, 1]],
+      [:hash, [:lit, :a], [:lvar, :a]]]
   end
 end
