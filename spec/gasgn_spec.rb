412af30df52c06134d61f8dcf995d85c798c1ac6
Brian Shirai <brixen@gmail.com>
2014-01-03 16:03:05 -0800

    Upadted gasgn specs.

--- spec/gasgn_spec.rb
+++ spec/gasgn_spec.rb
@@ -1,13 +1,9 @@
 describe "A Gasgn node" do
-  relates "$x = 42" do
-    parse do
-      [:gasgn, :$x, [:lit, 42]]
-    end
+  parse "$x = 42" do
+    [:gasgn, :$x, [:lit, 42]]
   end
 
-  relates "$a = *[1]" do
-    parse do
-      [:gasgn, :$a, [:svalue, [:splat, [:array, [:lit, 1]]]]]
-    end
+  parse "$a = *[1]" do
+    [:gasgn, :$a, [:splat, [:array, [:lit, 1]]]]
   end
 end
