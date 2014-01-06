6f5f48d8fe1c373c8023373672a1469b3ac490b3
Brian Shirai <brixen@gmail.com>
2014-01-05 21:48:53 -0800

    Updated or specs.

--- spec/or_spec.rb
+++ spec/or_spec.rb
@@ -1,35 +1,29 @@
 describe "An Or node" do
-  relates "(a or b)" do
-    parse do
-      [:or, [:call, nil, :a, [:arglist]], [:call, nil, :b, [:arglist]]]
-    end
+  parse "(a or b)" do
+    [:or, [:call, nil, :a, [:arglist]], [:call, nil, :b, [:arglist]]]
   end
 
-  relates "((a || b) || (c && d))" do
-    parse do
-      [:or,
-       [:or, [:call, nil, :a, [:arglist]], [:call, nil, :b, [:arglist]]],
-       [:and, [:call, nil, :c, [:arglist]], [:call, nil, :d, [:arglist]]]]
-    end
+  parse "((a || b) || (c && d))" do
+    [:or,
+     [:call, nil, :a, [:arglist]],
+     [:or,
+      [:call, nil, :b, [:arglist]],
+      [:and, [:call, nil, :c, [:arglist]], [:call, nil, :d, [:arglist]]]]]
   end
 
-  relates "((a or b) or (c and d))" do
-    parse do
-      [:or,
-       [:or, [:call, nil, :a, [:arglist]], [:call, nil, :b, [:arglist]]],
-       [:and, [:call, nil, :c, [:arglist]], [:call, nil, :d, [:arglist]]]]
-    end
+  parse "((a or b) or (c and d))" do
+    [:or,
+     [:call, nil, :a, [:arglist]],
+     [:or,
+      [:call, nil, :b, [:arglist]],
+      [:and, [:call, nil, :c, [:arglist]], [:call, nil, :d, [:arglist]]]]]
   end
 
-  relates "() or a" do
-    parse do
-      [:or, [:nil], [:call, nil, :a, [:arglist]]]
-    end
+  parse "() or a" do
+    [:or, [:nil], [:call, nil, :a, [:arglist]]]
   end
 
-  relates "a or ()" do
-    parse do
-      [:or, [:call, nil, :a, [:arglist]], [:nil]]
-    end
+  parse "a or ()" do
+    [:or, [:call, nil, :a, [:arglist]], [:nil]]
   end
 end
